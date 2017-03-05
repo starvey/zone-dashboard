667959767e784d2de12c4b123713e05c678ef786
Steven <steven.harvey679@gmail.com>
2017-03-05 18:55:38 -0500

    Time alert

--- src/components/StoryBoard.vue
+++ src/components/StoryBoard.vue
@@ -8,7 +8,9 @@
     <div class="story-board__row" v-for="story in stories">
       <div class="story-board__column-header-cell" :style="{width: columnWidth}">{{ story.subject }}</div>
       <div class="story-board__cell" v-for="(status, statusId) in taskStatuses" :style="{width: columnWidth}">
-        <div v-for="(task, taskId) in tasksForStatus(story.id, statusId)" class="story-board__task">{{task.subject}}</div>
+        <div v-for="(task, taskId) in tasksForStatus(story.id, statusId)" :data-days-without-update="daysWithoutUpdateForTaskId(taskId)" class="story-board__task" :class="classNameForTaskId(taskId)">
+          {{task.subject}}
+        </div>
       </div>
     </div>
   </div>
@@ -33,6 +35,21 @@ export default {
         return agg
       }, {})
       return tasks
+    },
+    daysWithoutUpdateForTaskId (taskId) {
+      const task = this.$store.state.tasks[taskId]
+      const lastModifiedDate = new Date(task.modified_date)
+      const todayDate = new Date()
+      const timeDiff = Math.abs(todayDate.getTime() - lastModifiedDate.getTime())
+      const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) - 1
+      return diffDays
+    },
+    classNameForTaskId (taskId) {
+      const daysWithoutUpdate = this.daysWithoutUpdateForTaskId(taskId)
+      return {
+        'story-board__task--no-update-warning': daysWithoutUpdate === 1,
+        'story-board__task--no-update-error': daysWithoutUpdate > 1
+      }
     }
   }
 }
@@ -64,8 +81,34 @@ export default {
     border-radius: 5px;
     border-bottom: 1px solid #BBBBBB;
     border-right: 1px solid #BBBBBB;
+    position: relative;
     &:not(:last-child) {
-      margin-bottom: 10px;
+      margin-bottom: 15px;
+    }
+
+    &.story-board__task--no-update-warning, &.story-board__task--no-update-error {
+      &::before {
+        content: attr(data-days-without-update);
+        display: block;
+        position: absolute;
+        height: 20px;
+        min-width: 20px;
+        line-height: 20px;
+        border-radius: 10px;
+        right: -10px;
+        top: -10px;
+        text-align: center;
+        vertical-align: middle;
+        box-sizing: border-box;
+        padding: 0 5px;
+      }
+    }
+    &.story-board__task--no-update-warning::before {
+      background: orange;
+    }
+    &.story-board__task--no-update-error::before {
+      background: red;
+      color: white;
     }
   }
 </style>
